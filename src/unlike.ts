d1e012e63df729d8030d491458d619d6a374638b
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-01 16:40:24 +0300

    squash!

---
+++ src/unlike.ts
@@ -0,0 +1,28 @@
+(() => {
+	const init = () => {
+		const array = new Set<HTMLElement>();
+
+		document
+			.querySelectorAll<HTMLElement>('[data-testid="unlike"]')
+			.forEach((el) => array.add(el));
+
+		let k = 0;
+
+		for (const el of array) {
+			k += 1000;
+
+			setTimeout(() => {
+				el.scrollIntoView();
+				array.delete(el);
+				el.click();
+			}, 1000 + k);
+		}
+
+		array.size === 0 ? history.go(0) : {};
+	};
+
+	init();
+
+	setInterval(() => window.scrollTo(0, window.scrollY + 500), 15000);
+	setInterval(() => init(), 10000);
+})();
