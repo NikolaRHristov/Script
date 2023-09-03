f8d8a5370b73d99995055de127435406381c5b78
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-03 10:45:28 +0300

    squash!

--- Source/Scrape.ts
+++ Source/Scrape.ts
@@ -1,17 +1,19 @@
 (() => {
 	setInterval(() => {
 		document
-		.querySelector<HTMLElement>('[data-tooltip="Add to collection"]')
-		?.click();
+			.querySelector<HTMLElement>('[data-tooltip="Add to collection"]')
+			?.click();
 
 		setTimeout(() => {
-			document.querySelector<HTMLElement>('.add-to-saved')?.click();
-		
+			document.querySelector<HTMLElement>(".add-to-saved")?.click();
+
 			setTimeout(() => {
-				document.querySelector<HTMLElement>('.upnext-item.active + .upnext-item img')?.click();
+				document
+					.querySelector<HTMLElement>(
+						".upnext-item.active + .upnext-item img"
+					)
+					?.click();
 			}, 1000);
 		}, 500);
 	}, 3000);
 })();
-
-
