338e9cce29da93293b38c91cf3056d1eb00e12a0
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-29 01:20:48 +0300

    squash!

--- Target/Scrape.js
+++ Target/Scrape.js
@@ -1,16 +1,1 @@
-(function () {
-    setInterval(function () {
-        var _a;
-        (_a = document
-            .querySelector('[data-tooltip="Add to collection"]')) === null || _a === void 0 ? void 0 : _a.click();
-        setTimeout(function () {
-            var _a;
-            (_a = document.querySelector(".add-to-saved")) === null || _a === void 0 ? void 0 : _a.click();
-            setTimeout(function () {
-                var _a;
-                (_a = document
-                    .querySelector(".upnext-item.active + .upnext-item img")) === null || _a === void 0 ? void 0 : _a.click();
-            }, 1000);
-        }, 500);
-    }, 3000);
-})();
+setInterval(()=>{document.querySelector('[data-tooltip="Add to collection"]')?.click(),setTimeout(()=>{document.querySelector(".add-to-saved")?.click(),setTimeout(()=>{document.querySelector(".upnext-item.active + .upnext-item img")?.click()},1e3)},500)},3e3);
