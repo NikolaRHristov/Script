e949a5f67018e3342462d03ae18283bd43a3f572
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-11 02:01:51 +0300

    squash!

---
+++ Target/Reload.js
@@ -0,0 +1,1 @@
+(()=>{const e=()=>{document.querySelectorAll('[data-testid="ScrollSnap-List"] > [role="tab"]').forEach((t,n)=>{new Number(new URL(document.location.href).searchParams.get("Index"))})};e(),setInterval(()=>window.scrollTo(0,window.scrollY+500),15e3),setInterval(()=>e(),8e4)})();
