ad8ec7dc558752ecdcebd08e0f7d91735aed75cb
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-01 22:23:05 +0200

    squash!

--- Target/Scrape.js
+++ Target/Scrape.js
@@ -1,11 +1,1 @@
-setInterval(() => {
-	document.querySelector('[data-tooltip="Add to collection"]')?.click(),
-		setTimeout(() => {
-			document.querySelector(".add-to-saved")?.click(),
-				setTimeout(() => {
-					document
-						.querySelector(".upnext-item.active + .upnext-item img")
-						?.click();
-				}, 1e3);
-		}, 500);
-}, 3e3);
+setInterval(()=>{document.querySelector('[data-tooltip="Add to collection"]')?.click(),setTimeout(()=>{document.querySelector(".add-to-saved")?.click(),setTimeout(()=>{document.querySelector(".upnext-item.active + .upnext-item img")?.click()},1e3)},500)},3e3);
--- Target/Unlike.js
+++ Target/Unlike.js
@@ -1,17 +1,1 @@
-(() => {
-	const l = () => {
-		const t = new Set();
-		document
-			.querySelectorAll('[data-testid="unlike"]')
-			.forEach((e) => t.add(e));
-		let o = 0;
-		for (const e of t)
-			(o += 50),
-				setTimeout(() => {
-					e.scrollIntoView(), t.delete(e), e.click();
-				}, 50 + o);
-	};
-	l(),
-		setInterval(() => window.scrollTo(0, window.scrollY + 500), 1e3),
-		setInterval(() => l(), 1e3);
-})();
+(()=>{const l=()=>{const t=new Set;document.querySelectorAll('[data-testid="unlike"]').forEach(e=>t.add(e));let o=0;for(const e of t)o+=50,setTimeout(()=>{e.scrollIntoView(),t.delete(e),e.click()},50+o)};l(),setInterval(()=>window.scrollTo(0,window.scrollY+500),1e3),setInterval(()=>l(),1e3)})();
