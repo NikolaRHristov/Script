2c2a6e9691dd5582313c7b835dd1cc1d879f615a
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-15 11:15:31 +0200

    squash!

--- Target/Reload.js
+++ Target/Reload.js
@@ -1,1 +1,14 @@
-(()=>{const e=()=>{document.querySelectorAll('[data-testid="ScrollSnap-List"] > [role="tab"]').forEach((t,n)=>{new Number(new URL(document.location.href).searchParams.get("Index"))})};e(),setInterval(()=>window.scrollTo(0,window.scrollY+500),15e3),setInterval(()=>e(),8e4)})();
+(() => {
+	const e = () => {
+		document
+			.querySelectorAll('[data-testid="ScrollSnap-List"] > [role="tab"]')
+			.forEach((t, n) => {
+				new Number(
+					new URL(document.location.href).searchParams.get("Index")
+				);
+			});
+	};
+	e(),
+		setInterval(() => window.scrollTo(0, window.scrollY + 500), 15e3),
+		setInterval(() => e(), 8e4);
+})();
